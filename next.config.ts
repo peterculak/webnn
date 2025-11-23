dab4dc3bd1cefc17de843fdcca2697d69bbe9abc
Peter Culak <peter.culak@outlook.com>
2025-11-23 15:42:07 +0000

    change base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/webnn',
+  basePath: '/webllm',
   images: {
     unoptimized: true,
   },
